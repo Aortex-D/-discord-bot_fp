f387a59d5ed28053e442153667d2b850fc067795
Aortex-D <ayushgamerz0003@gmail.com>
2025-06-25 16:39:40 +0530

    Updated main.py | Updted log msg

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def on_ready():
             if update_channel:
                 embed = discord.Embed(
                     title="🚀 Bot has been updated",
-                    description="**Changes**\n- Added /leaderboard",
+                    description="**Changes**\n- Updated /modify -> Now deletes data from db once user is kicked.",
                     color=discord.Color.green()
                 )
                 embed.set_footer(text=f"Updated by _Suspected_")
